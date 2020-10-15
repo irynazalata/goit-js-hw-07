7473b8f379766aa1352d0672459fda4a8f582dd3
Iryna Zalata <iryna.zalata@gmail.com>
2020-10-15 14:05:15 +0300

    Add Task 1 and Task 2

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+// TASK 1 -----------------------
+
+// const listRef = document.querySelectorAll('.item')
+// const categoryAmount = listRef.length
+// console.log(`У списку ${categoryAmount} категорії.`)
+
+// const headingRef = document.querySelectorAll('h2')
+// headingRef.forEach(el => console.log(`Категорія: ${el.textContent}\nКількість елементів: ${el.nextElementSibling.children.length}`))
---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+// TASK 2 -----------------------
+
+// const ingredients = [
+//   'Картопля',
+//   'Гриби',
+//   'Часник',
+//   'Помідори',
+//   'Зелень',
+//   'Приправи',
+// ];
+
+// for (let el of ingredients) {
+//   const listItemRef = document.createElement('li')
+//   const listRef = document.querySelector('#ingredients')
+//   listItemRef.textContent = el
+//   listRef.appendChild(listItemRef)
+// }
