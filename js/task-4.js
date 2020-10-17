01bf83f453660d9e22e26086559faa795a84b702
Iryna Zalata <iryna.zalata@gmail.com>
2020-10-17 13:24:20 +0300

    Add variant 2 of task solution

--- js/task-4.js
+++ js/task-4.js
@@ -1,32 +1,32 @@
 // TASK 4 -----------------------
 
-// const incrementButton = document.querySelector('button[data-action="increment"]');
-// const decrementButton = document.querySelector('button[data-action="decrement"]');
-// const value = document.querySelector('#value')
+const incrementButton = document.querySelector('button[data-action="increment"]');
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector('#value')
 
-// const increment = function () {
-//   value.textContent = +value.textContent + 1
-// }
+const increment = function () {
+  value.textContent = +value.textContent + 1
+}
 
-// const decrement = function () {
-//   value.textContent = +value.textContent - 1
-// }
+const decrement = function () {
+  value.textContent = +value.textContent - 1
+}
 
-// incrementButton.addEventListener('click', increment);
-// decrementButton.addEventListener('click', decrement);
+incrementButton.addEventListener('click', increment);
+decrementButton.addEventListener('click', decrement);
 
 // -------------------------- Variant 2 --------------------------------
-const container = document.querySelector('#counter');
-const value = document.querySelector('#value');
+// const container = document.querySelector('#counter');
+// const value = document.querySelector('#value');
 
-container.addEventListener('click', (event) => {
-  switch (event.target.getAttribute('data-action')) {
-    case 'increment':
-      value.textContent = +value.textContent + 1;
-      break;
+// container.addEventListener('click', (event) => {
+//   switch (event.target.getAttribute('data-action')) {
+//     case 'increment':
+//       value.textContent = +value.textContent + 1;
+//       break;
 
-    case 'decrement':
-      value.textContent = +value.textContent - 1;
-      break;
-  };
-});
+//     case 'decrement':
+//       value.textContent = +value.textContent - 1;
+//       break;
+//   };
+// });
